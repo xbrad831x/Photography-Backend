a40feafd3128b60160e33538a79704aebd3385e1
Brandon <mercadobrandon33@yahoo.com>
2018-10-08 22:09:50 -0700

    Fixed tiny bug

--- dc_api/settings.py
+++ dc_api/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'dc_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=true)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
 
 
 
