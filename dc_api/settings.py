f465eb90d4a21e5e27a6b23ba334f9f8c93fbb8f
Brandon <mercadobrandon33@yahoo.com>
2018-10-09 20:14:27 -0700

    took out www

--- dc_api/settings.py
+++ dc_api/settings.py
@@ -16,8 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PREPEND_WWW = True
-BASE_URL = "https://www.donna-photography-api.herokuapp.com"
+BASE_URL = "https://donna-photography-api.herokuapp.com"
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
