1c2b5d633c6a6ca1d226f1c0b7f1b1479881255c
Brandon <mercadobrandon33@yahoo.com>
2018-10-16 17:46:56 -0700

    Added picture upload for reviews

--- reviews/models.py
+++ reviews/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Review(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(upload_to= 'review_pic/')
     created_at = models.DateTimeField(auto_now_add=True)
